920d778288ad6fca42f1d66bf0de4c2845683297
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-11 20:49:56 +0900

    feat : Header bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import React from 'react'
+import Header from './components/Header';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <p id='p'>h</p>
     </div>
   );
 }
 
-export default App;
+// eslint-disable-next-line no-lone-blocks
+{/* <BrowserRouter>
+  <nav>
+    
+    <NavLink to="/">Home</NavLink>  NavLink : 선택된 요소 class에 active 자동 생성 
+    <NavLink to="test">Test</NavLink>
+  </nav>
+  <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='test' element={<Test />} />
+  </Routes>
+</BrowserRouter> */}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='header-items'>
+                <button className='shes-thema header-btn'>SHES' THEMA</button>
+                <button className='down header-btn'>DOWNLOAD</button>
+            </div>
+        </div>
+    )
+}
