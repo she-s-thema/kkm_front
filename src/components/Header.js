5e08b7c830ec77e97a98902e621d460fcb24c3a0
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-21 20:33:01 +0900

    FEAT :: login, signup 마크업

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ export default function Header() {
     return (
         <header className='header'>
             <div className='header-content'>
-                <img className='text-logo-img' src='./text-logo.png' alt='logo' />
+                <img className='text-logo-img' src='./imgs/text-logo.png' alt='logo' />
                 <div className='links'>
                     <NavLink className="header-link" to="/">홈</NavLink>
                     <NavLink className="header-link" to="/chat">채팅</NavLink>
                     <NavLink className="header-link" to="/mypage">마이페이지</NavLink>
-                    <NavLink className="header-link" to="/login">로그인 / 회원가입</NavLink>
+                    <NavLink className="header-link" to="/login">로그인</NavLink>
                 </div>
             </div>
         </header>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Login() {
     return (
-        <p>login</p>
+        <main className='login'>
+            <div className='login-main'>
+                <img className='no-text-logo' alt='no-text-logo' src='./imgs/logo.png'/>
+                <form>
+                    <input type="email" placeholder='이메일을 입력해주세요.'/>
+                    <input type="password" placeholder='비밀번호를 입력해주세요.'/>
+                    <button type="submit">로그인</button>
+                </form>
+                <NavLink className="go-to-signup" to="/signup">회원이 아니신가요?</NavLink>
+            </div>
+        </main>
     )
 }
