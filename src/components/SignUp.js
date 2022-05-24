7f6cd19a7b523af4facf897d70df3e1bc4580757
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-24 22:35:17 +0900

    FEAT :: 닉네임 글자 수 확인, Error 메세지 show

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 export default function SignUp() {
+    const [userData, setUserData] = useState({
+        email:"",
+        nickname:"",
+        password:""
+    })
+    const [errMsg, setErrMsg] = useState("");
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        console.log(name, value);
+        setUserData(prev => {
+            return {
+                ...prev,
+                [name]:[value]
+            }
+        }
+        )
+    }
+
+    const checkData = (e) => {
+        if(userData.email === "" || userData.nickname === "" || userData.password === "") {
+            setErrMsg("정보가 입력되지 않았습니다.");
+            e.preventDefault();
+        }
+        else if(userData.nickname[0].length > 10 || userData.nickname[0].length < 2) {
+            setErrMsg("닉네임 설정 기준은 2 ~ 10자 사이입니다.")
+            e.preventDefault();
+        } else {
+            setErrMsg("")
+        }
+    }
+
     return (
         <main className='login'>
             <div className='login-main'>
                 <img className='no-text-logo' alt='no-text-logo' src='./imgs/logo.png'/>
-                <form>
-                    <input type="email" placeholder='이메일을 입력해주세요.'/>
-                    <input type="text" placeholder='닉네임을 입력해주세요.'/>
-                    <input type="password" placeholder='비밀번호를 입력해주세요.'/>
+                <form onSubmit={checkData}>
+                    <input name="email" type="email" placeholder='이메일을 입력해주세요.' onChange={handleChange}/>
+                    <input name="nickname" type="text" placeholder='닉네임을 입력해주세요.' onChange={handleChange}/>
+                    <input name="password" type="password" placeholder='비밀번호를 입력해주세요.' onChange={handleChange}/>
                     <button type="submit">회원가입</button>
                 </form>
+                <span>{errMsg}</span>
             </div>
         </main>
     )
