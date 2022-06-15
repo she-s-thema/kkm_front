99841d969e3812607c2f87fd169b90ac02c9402b
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-15 20:19:35 +0900

    FEAT :: change link to a tag, logo img
    Link를 쓰니 url이 합쳐져 a 태그로 바꾸고, 로고 사진 바꿈

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,17 @@
-import React, { useEffect }  from 'react'
+import React from 'react'
 
-export default function Login() {
+export default function Login(props) {
     const REST_API_KEY = "9240bec26b639066d5ac5afdbaeb6bb0";
     const REDIRECT_URI =  "http://localhost:3000/auth/kakao/callback";
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     return (
-        <main className='login'>
-            <div className='login-main'>
-                <img className='no-text-logo' alt='no-text-logo' src='./imgs/logo.png'/>
-                <a href={KAKAO_AUTH_URL}>카카오로 로그인하기</a>
-            </div>
-        </main>
+        <section onClick={props.stopShowLogin} className='loging'>
+            <main className='login'>
+                <div className='login-main'>
+                    <img className='no-text-logo' alt='no-text-logo' src='./imgs/logo-with-text.png'/>
+                    <a className='login-by-kakao' href={KAKAO_AUTH_URL}>카카오 로그인</a>
+                </div>
+            </main>
+        </section>
     )
 }
