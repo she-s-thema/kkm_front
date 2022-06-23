c3a6393a017bde48fcd8e9972a378678d16348b4
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-23 20:06:11 +0900

    Fix - 경로 설정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { makeServer } from './server';
+import { makeServer } from './server/server';
 
 if(process.env.NODE_ENV === 'development') {
   makeServer({ environment: 'development' });
