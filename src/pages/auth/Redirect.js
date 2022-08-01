a012a8679623b397df4fb18c2156110fd6d262a3
kimhyerin0909 <khyerin0909@gmail.com>
2022-08-02 01:15:52 +0900

    Feat - kakao login, move pages according to ID

--- src/pages/auth/Redirect.js
+++ src/pages/auth/Redirect.js
@@ -3,12 +3,26 @@ import queryString from "query-string";
 import axios from 'axios';
 
 export const Redirect = () => {
+
+    const kakaoLogin =  async(code) => {
+        const res =  await axios.post(`/user/kakaoLogin?code=${code}`)
+        .then(res =>{
+            if (res.data["id"] === 1) {
+                // user_id, nickname, k_id, k_img_url, k_email
+                console.log(res.data);
+                // window.location.href = "/moreInfo";
+            } else {
+                console.log(res.data);
+                // window.location.href = "/";
+            }
+        });
+    }
+
     useEffect(() => {
         const sp = window.location.search;
         const query = queryString.parse(sp); // params 값을 key-value로 가져옴
 
-        axios.post(`/kakaoLogin?code=${query.code}`)
-        .then(res => console.log(res));
+        kakaoLogin(query.code);
     })
     return <></>
 }
