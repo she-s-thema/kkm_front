16c3655852f1a30991573a969d8236d5d5e50286
kimhyerin0909 <khyerin0909@gmail.com>
2022-08-17 21:51:46 +0900

    Feat - 닉네임 설정

--- src/pages/auth/MoreInfo.js
+++ src/pages/auth/MoreInfo.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useRecoilState } from "recoil";
+import { userInfo } from "../../data/atom";
 
 export const MoreInfo = () => {
+  const [user, setUser] = useRecoilState(userInfo);
+  const [nickname, setNickname] = useState("");
+
+  const setName = ({ target: { value } }) => {
+    setNickname(value);
+  };
+
+  const signUp = () => {
+    // setUser(prev => {userInfo: {
+    //   ...userInfo
+    // }});
+    // console.log(user);
+  };
+
   return (
-    <div>MoreInfo</div>
-  )
-}
+    <div>
+      <div>
+        <span>닉네임 설정</span>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="닉네임 설정 2 ~ 10자"
+          onChange={setName}
+        />
+      </div>
+      <div>
+        <button type="button" onClick={signUp}>
+          회원가입
+        </button>
+      </div>
+    </div>
+  );
+};
