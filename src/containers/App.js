b89dd23a8c62a5a7ddc62a6f7551becc653fc961
{yunaeun} <naeun2964@naver.com>
2022-08-18 21:22:30 +0900

    Feat - component of /chat

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import { Header } from '../components/Header';
-import { Home } from '../pages/home/Home';
-import { Chat } from '../pages/chat/Chat'
-import { Post } from '../pages/post/Post'
-import { Mypage } from '../pages/mypage/Mypage';
-import { Login } from '../pages/auth/Login';
-import { Redirect } from '../pages/auth/Redirect';
-import { MoreInfo } from '../pages/auth/MoreInfo';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { Header } from "../components/Header";
+import { Home } from "../pages/home/Home";
+import { Post } from "../pages/post/Post";
+import { Mypage } from "../pages/mypage/Mypage";
+import { Login } from "../pages/auth/Login";
+import { Redirect } from "../pages/auth/Redirect";
+import { MoreInfo } from "../pages/auth/MoreInfo";
+import { ChatList } from "../pages/chat/ChatList";
 
 export default function App() {
   return (
     <RecoilRoot>
       <BrowserRouter>
-      <Header />
+        <Header />
         <Routes>
-          <Route exact path='/'  element={<Home />}/>
-          <Route path='/chat' element={<Chat /> }/>
-          <Route path='/posting' element={<Post /> }/>
-          <Route path='/mypage' element={<Mypage /> }/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/auth/kakao/*' element={<Redirect />} />
-          <Route path='/login/moreInfo' element={<MoreInfo />}/>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatList />} />
+          <Route path="/posting" element={<Post />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/auth/kakao/*" element={<Redirect />} />
+          <Route path="/login/moreInfo" element={<MoreInfo />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
