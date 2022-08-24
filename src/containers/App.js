ab20648ce588a9b083feee8a9f50bb170f385835
{yunaeun} <naeun2964@naver.com>
2022-08-24 21:22:40 +0900

    zz

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,6 +9,7 @@ import { Login } from "../pages/auth/Login";
 import { Redirect } from "../pages/auth/Redirect";
 import { MoreInfo } from "../pages/auth/MoreInfo";
 import { ChatList } from "../pages/chat/ChatList";
+import { Chat } from "../pages/chat/Chat";
 
 export default function App() {
   return (
@@ -17,12 +18,13 @@ export default function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/chat" element={<ChatList />} />
+          <Route path="/chatlist" element={<ChatList />} />
           <Route path="/posting" element={<Post />} />
           <Route path="/mypage" element={<Mypage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/auth/kakao/*" element={<Redirect />} />
           <Route path="/login/moreInfo" element={<MoreInfo />} />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
--- src/pages/chat/ChatList.js
+++ src/pages/chat/ChatList.js
@@ -1,5 +1,7 @@
 import userEvent from "@testing-library/user-event";
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import { Chat } from "./Chat";
 import styled from "styled-components";
 import User from "../../data/chatUser.json";
 
@@ -7,31 +9,46 @@ export const ChatList = () => {
   return (
     <div>
       <ChatBox>
-        <ChatCards>
-          {User.map((data) => {
-            const chatcard = (
-              <ChatCard key={data.id}>
-                <ChatProfile src={data.profile} />
-              </ChatCard>
-            );
-            return chatcard;
-          })}
-        </ChatCards>
+        <Link to="./chat">
+          <ChatCards>
+            {User.map((data) => {
+              const chatcard = (
+                <ChatCard key={data.id}>
+                  <ChatProfile src={data.profile} />
+                  <ChatName>{data.name}</ChatName>
+                  <ChatContent>{data.content}</ChatContent>
+                  ChatButton({data.condition})
+                </ChatCard>
+              );
+              return chatcard;
+            })}
+          </ChatCards>
+        </Link>
       </ChatBox>
     </div>
   );
 };
 
+function ChatButton(data) {
+  if (data == "거래전") {
+    return <ChatPink>{data}</ChatPink>;
+  } else if (data == "거래중") {
+    return <ChatYel>{data}</ChatYel>;
+  } else {
+    return <ChatBlue>{data}</ChatBlue>;
+  }
+}
+
 const ChatBox = styled.div`
   display: flex;
-  width: 400px;
-  height: 400px;
-  border: 1px #c4c4c4;
-  margin-right: 30%;
+  width: 25%;
+  height: 420px;
+  border: 2px;
+  background-color: #c4c4c4;
 `;
 
 const ChatCards = styled.div`
-  display: flex;
+  display: block;
   width: 100%;
 `;
 
@@ -48,12 +65,41 @@ const Name = styled.div`
 `;
 
 const ChatCard = styled.div`
-  display: flex;
+  display: block;
+  height: 40%;
+  width: 100%;
+  padding-top: 10px;
+  padding-bottom: 10px;
 `;
 
 const ChatProfile = styled.img`
   display: flex;
-  width: 20px;
-  height: 20px;
+  width: 40px;
+  height: 40px;
   border-radius: 20px;
+`;
+
+const ChatContent = styled.h6`
+  color: black;
+  text-decoration: none;
+`;
+
+const ChatName = styled.h4`
+  color: black;
+  text-decoration: none;
+`;
+
+const ChatPink = styled.button`
+  border: 1px #ffb7d2;
+  color: #ffb7d2;
+`;
+
+const ChatBlue = styled.button`
+  border: 1px #a5d9ff;
+  color: #ffb7d2;
+`;
+
+const ChatYel = styled.button`
+  border: 1px #ffcc66;
+  color: #ffb7d2;
 `;
