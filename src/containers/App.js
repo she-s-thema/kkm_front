b561d217c272fc964f1f2d97b6369811543967e8
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-02 13:49:15 +0900

    Feat - chat route 수정, 게시글 id로 연결 route

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Header } from "../components/Header";
-import { Home } from "../pages/home/Home";
 import { Post } from "../pages/post/Post";
 import { Mypage } from "../pages/mypage/Mypage";
 import { Login } from "../pages/auth/Login";
 import { Redirect } from "../pages/auth/Redirect";
 import { MoreInfo } from "../pages/auth/MoreInfo";
 import { ChatList } from "../pages/chat/ChatList";
-import { Chat } from "../pages/chat/Chat";
 import { IsLogin } from "../components/IsLogin";
+import { PostDetail } from "../pages/home/PostDetail";
+import queryString from "query-string";
+import { Chat } from "../pages/chat/Chat";
 
 export default function App() {
   return (
@@ -19,13 +20,14 @@ export default function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<IsLogin />} />
-          <Route path="/chatlist" element={<ChatList />} />
+          <Route path="/chat" element={<ChatList />} />
+          <Route path="/chat/:id" element={<Chat />} />
           <Route path="/posting" element={<Post />} />
           <Route path="/mypage" element={<Mypage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/auth/kakao/*" element={<Redirect />} />
           <Route path="/login/moreInfo" element={<MoreInfo />} />
-          <Route path="/chat" element={<Chat />} />
+          <Route path="/post/:id" element={<PostDetail />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
