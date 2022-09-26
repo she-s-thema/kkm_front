b4467e4fc97d82581b618e87d885b93260192d92
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-27 00:39:32 +0900

    Feat - react-query

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Header } from "../components/Header";
@@ -10,28 +10,32 @@ import { MoreInfo } from "../pages/auth/MoreInfo";
 import { ChatList } from "../pages/chat/ChatList";
 import { IsLogin } from "../components/IsLogin";
 import { PostDetail } from "../pages/home/PostDetail";
-import queryString from "query-string";
 import { Chat } from "../pages/chat/Chat";
 import { Test } from "../components/Test";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
     <RecoilRoot>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<IsLogin />} />
-          <Route path="/chat" element={<ChatList />} />
-          <Route path="/chat/:id" element={<Chat />} />
-          <Route path="/posting" element={<Post />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/auth/kakao/*" element={<Redirect />} />
-          <Route path="/login/moreInfo" element={<MoreInfo />} />
-          <Route path="/post/:id" element={<PostDetail />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<IsLogin />} />
+            <Route path="/chat" element={<ChatList />} />
+            <Route path="/chat/:id" element={<Chat />} />
+            <Route path="/posting" element={<Post />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/auth/kakao/*" element={<Redirect />} />
+            <Route path="/login/moreInfo" element={<MoreInfo />} />
+            <Route path="/post/:id" element={<PostDetail />} />
+            <Route path="/test" element={<Test />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
