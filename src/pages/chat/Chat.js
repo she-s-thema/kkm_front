81b37eb625ad5d4123b07698613fd1d676d6fcbe
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-04 19:43:21 +0900

    Feat - 채팅 보여주고, 보내기

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,43 +1,99 @@
-import userEvent from "@testing-library/user-event";
-import React from "react";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import React, { useMemo, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
-import User from "../../data/currentUser.json";
+import db from "../../config/firebaseConfig";
+import { userInfo } from "../../data/user";
 
 export const Chat = () => {
+  const { ch_id } = useParams();
+  const user = useRecoilValue(userInfo);
+  const user_id = user["user_id"];
+  const input = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const [sendText, setSendText] = useState("");
+
+  const getChatDatas = () => {
+    const q = query(
+      collection(db, "channels", ch_id, "chat"),
+      orderBy("sendAt")
+    );
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      setMessages([]);
+      querySnapshot.forEach((doc) => {
+        setMessages((prev) => [...prev, doc.data()]);
+      });
+    });
+  };
+
+  const sendChat = async () => {
+    const newChatRef = doc(collection(db, "channels", ch_id, "chat"));
+    const channelRef = doc(db, "channels", newChatRef._path["segments"][1]);
+    const sendTime = new Date();
+    input.current.value = "";
+
+    await setDoc(newChatRef, {
+      content: sendText,
+      from_id: user_id,
+      sendAt: sendTime,
+      id: newChatRef.id,
+    });
+
+    await updateDoc(channelRef, {
+      sendAt: sendTime,
+    });
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      sendChat();
+    }
+  };
+
+  useMemo(() => getChatDatas(), []);
+
   return (
     <div>
-      <ChatBox>
-        <ChatHead>
-          <Profile src={User.profile} />
-          <Name>{User.name}</Name>
-        </ChatHead>
-      </ChatBox>
+      {messages &&
+        messages.map((data) =>
+          data.from_id !== user_id ? (
+            <div key={data.id}>
+              <Opponent>{data.from_id}</Opponent>
+              <Opponent>{data.content}</Opponent>
+              <Opponent>{data.sendAt.toDate().toString()}</Opponent>
+            </div>
+          ) : (
+            <div key={data.id}>
+              <span>{data.from_id}</span>
+              <span>{data.content}</span>
+              <span>{data.sendAt.toDate().toString()}</span>
+            </div>
+          )
+        )}
+      <div>
+        <input
+          ref={input}
+          onKeyPress={handleKeyPress}
+          type="text"
+          placeholder="채팅 입력"
+          onChange={(e) => setSendText(e.target.value)}
+        />
+        <button onClick={sendChat}>전송</button>
+      </div>
     </div>
   );
 };
 
-const ChatBox = styled.div`
-  display: flex;
-  height: 400px;
-  width: 400px;
-  border: 1px #c4c4c4;
-`;
-
-const ChatHead = styled.div`
-  display: flex;
-  width: 100%;
-  height: 50px;
-  margin-right: 30%;
-`;
-
-const Profile = styled.div`
-  display: flex;
-  width: 20px;
-  height: 20px;
-  border-radius: 20px;
-`;
-
-const Name = styled.div`
-  display: flex;
-  color: black;
+const Opponent = styled.span`
+  color: red;
 `;
