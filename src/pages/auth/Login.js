ff66c03ec1750d00f8a220fe0fe6291725e33ee3
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-04 19:40:24 +0900

    Use - .env variable for security

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { REST_API_KEY, REDIRECT_URI } from "../../config/kakaoLoginConfig";
 
 export const Login = () => {
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
   return (
     <main>
       <div>
-        {/* <img/> */}
         <a href={KAKAO_AUTH_URL}>카카오로 로그인하기</a>
       </div>
     </main>
