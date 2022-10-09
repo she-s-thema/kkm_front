b41dedfdfc86a40a0b526a256d8f20c803a06f0f
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-09 20:34:25 +0900

    Feat - 최근 보낸 순으로 채팅방 정렬

--- src/pages/chat/Channels.js
+++ src/pages/chat/Channels.js
@@ -1,38 +1,70 @@
 import { collection, onSnapshot, query, where } from "firebase/firestore";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import db from "../../config/firebaseConfig";
 import { userInfo } from "../../data/user";
 
 export const Channels = () => {
+  const [channels, setChannels] = useState([]);
   const [loanerChat, setLoanerChat] = useState([]);
   const [ownerChat, setOwnerChat] = useState([]);
   const user = useRecoilValue(userInfo);
   const user_id = user["user_id"];
 
-  const getLoanerChats = () => {
-    const q = query(
+  const getChats = () => {
+    const q1 = query(
       collection(db, "channels"),
       where("loaner_id", "==", user_id)
     );
 
-    const unsub = onSnapshot(q, (querySnapshot) => {
+    const unsub1 = onSnapshot(q1, (querySnapshot) => {
       setLoanerChat([]);
       querySnapshot.forEach((doc) => {
         setLoanerChat((prev) => [...prev, doc.data()]);
       });
     });
+
+    const q2 = query(
+      collection(db, "channels"),
+      where("owner_id", "==", user_id)
+    );
+
+    const unsub2 = onSnapshot(q2, (querySnapshot) => {
+      setOwnerChat([]);
+      querySnapshot.forEach((doc) => {
+        setOwnerChat((prev) => [...prev, doc.data()]);
+      });
+    });
   };
 
-  useMemo(() => getLoanerChats(), []);
+  useMemo(() => {
+    getChats();
+  }, []);
+
+  useEffect(() => {
+    const channels = [];
+    loanerChat.forEach((doc) => channels.push(doc));
+    ownerChat.forEach((doc) => channels.push(doc));
+
+    channels.sort((a, b) => {
+      return a.sendAt > b.sendAt ? -1 : a.sendAt < b.sendAt ? 1 : 0;
+    });
+
+    setChannels(channels);
+  }, [ownerChat, loanerChat]);
 
   return (
     <div>
-      {loanerChat &&
-        loanerChat.map((data) => (
+      {channels &&
+        channels.map((data) => (
           <div key={data.id}>
-            <Link to={`${data.id}`}>{data.id}</Link>
+            <Link to={`${data.id}`}>
+              <span>
+                {data.loaner_id === user_id ? data.owner_id : data.loaner_id}
+              </span>
+              <span>{data.lastText}</span>
+            </Link>
           </div>
         ))}
     </div>
