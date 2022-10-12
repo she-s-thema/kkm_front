532fea75fac8b5f69e1a8136d518011b5f5291bb
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-12 19:16:27 +0900

    Style - 채팅 화면

--- src/pages/chat/chat/chat.style.js
+++ src/pages/chat/chat/chat.style.js
@@ -1,9 +1,39 @@
 import styled from "styled-components";
 
-export const Opponent = styled.span`
-  color: red;
+export const Frame = styled.div`
+  width: 65%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const ChatBox = styled.div`
-  width: 20%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+  background-color: white;
+  width: 100%;
+  height: 70vh;
+  margin-top: 10px;
+  overflow-y: scroll;
+  padding: 2%;
+`;
+export const ContentBox = styled.div`
+  display: flex;
+  justify-content: ${({ who }) => (who ? "flex-start" : "flex-end")};
+`;
+
+export const Content = styled.span`
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+  padding: 10px 19px;
+
+  color: black;
+  background-color: ${({ who }) => (who ? "white" : "#DFE6FF")};
+
+  border: ${({ who }) => who && "0.2px solid #cccccc"};
+  border-radius: ${({ who }) =>
+    who === "opponent" ? "100px 100px 100px 0px" : "100px 100px 0px 100px"};
 `;
