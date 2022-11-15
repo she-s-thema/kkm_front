67bfdeafc1b45bb4e3dd62595fea7682513fa565
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-15 11:54:05 +0900

    Fix - add of server

--- src/utils/CustomAxios.js
+++ src/utils/CustomAxios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
 import React from "react";
 
-export const CustomAxios = axios.create({ baseURL: "http://localhost:3031/" });
+export const CustomAxios = axios.create({
+  baseURL: "http://54.180.30.70:3031/",
+});
