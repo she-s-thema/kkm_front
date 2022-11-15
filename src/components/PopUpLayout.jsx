d114d6c15440dd289712692974e3612526a3eadc
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-15 11:53:37 +0900

    Style - pop up layout

---
+++ src/components/PopUpLayout.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const PopUpLayout = ({ children }) => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        background: " rgba(0, 0, 0, 0.53)",
+        top: 0,
+        left: 0,
+        zIndex: 4,
+      }}
+    >
+      <div
+        style={{
+          width: "30%",
+          position: "fixed",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "10px",
+          colomnGap: "5px",
+          backgroundColor: "white",
+          borderRadius: "15px",
+          padding: "2% 3%",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
