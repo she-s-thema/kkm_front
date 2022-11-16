ae6fb4c58bed99e2fb2e8dea420edf760808eaed
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-16 23:29:24 +0900

    Feat - update stage of promise

--- src/utils/promise.js
+++ src/utils/promise.js
@@ -2,12 +2,11 @@ import {
   collection,
   doc,
   getDoc,
-  getDocs,
-  limit,
   onSnapshot,
   orderBy,
   query,
   setDoc,
+  updateDoc,
 } from "firebase/firestore";
 import db from "../config/firebaseConfig";
 
@@ -19,33 +18,36 @@ export const makePromise = async (
   place,
   time,
   stage,
-  ch_id
+  ch_id,
+  user_id
 ) => {
   const newProRef = doc(collection(db, "channels", ch_id, "promise"));
-  stage === 0
+  stage === 1 || stage === 7
     ? await setDoc(newProRef, {
         id: newProRef.id,
         loaner_id: lid,
         owner_id: oid,
-        p_id: "first",
+        p_id: 1,
         period: {
           start_date: sdate,
           end_date: edate,
         },
         place: place,
         time: time,
-        state: 0,
+        stage: 1,
+        from_id: user_id,
         madeAt: new Date(),
       })
     : await setDoc(newProRef, {
         id: newProRef.id,
         loaner_id: lid,
         owner_id: oid,
-        p_id: "second",
+        p_id: 2,
         place: place,
         time: time,
-        state: 0,
+        stage: 4,
         madeAt: new Date(),
+        from_id: user_id,
       });
 };
 
@@ -58,15 +60,16 @@ export const getPromises = async (ch_id) => {
   onSnapshot(q, (qsn) => {
     if (!qsn.empty) {
       qsn.forEach((doc) => {
-        result.stage = 1;
+        result.stage = doc.data().stage;
         result.loaner_id = doc.data().loaner_id;
         result.owner_id = doc.data().owner_id;
         result.madeAt = doc.data().madeAt;
         result.p_id = doc.data().p_id;
         result.period = doc.data().period;
         result.place = doc.data().place;
-        result.state = doc.data().state;
         result.time = doc.data().time;
+        result.from_id = doc.data().from_id;
+        result.id = doc.data().id;
       });
     } else {
       const docRef = doc(db, "channels", ch_id);
@@ -78,4 +81,11 @@ export const getPromises = async (ch_id) => {
     }
   });
   return result;
+};
+
+export const updateStage = async (ch_id, p_id, stnum) => {
+  const proRef = doc(db, "channels", ch_id, "promise", p_id);
+  await updateDoc(proRef, {
+    stage: stnum,
+  });
 };
